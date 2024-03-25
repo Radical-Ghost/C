1c1954ea0ee2246ae30d49579bbe5968f5f46a80
Soham Mandavkar <radicalghost98@gmail.com>
2024-03-25 21:25:13 +0530

    .

--- Sorting Algorithms/Knuth_Morris_Pattern.c
+++ Sorting Algorithms/Knuth_Morris_Pattern.c
@@ -2,30 +2,29 @@
 #include <string.h>
 #define max 30
 
-int computeLPS(char *pat, int m, int *lps) {
-    int len = 0, i = 1;
-    lps[0] = 0;
+void KMP(char *txt, char *pat) {
+    int lps[max], i = 0, j = 0;
+    int n = strlen(txt), m = strlen(pat);
 
+    // Compute LPS array
+    int len = 0;
+    lps[0] = 0;
     while(i < m) {
         if(pat[i] == pat[len]) {
             lps[i] = ++len;
             i++;
         } else {
-            if(len != 0) len = lps[len - 1];
+            if(len != 0) 
+                len = lps[len - 1];
             else {
                 lps[i] = 0;
                 i++;
             }
         }
     }
-}
 
-int KMP(char *txt, char *pat) {
-    int lps[max], i = 0, j = 0;
-    int n = strlen(txt), m = strlen(pat);
-
-    computeLPS(pat, m, lps);
-
+    // KMP algorithm
+    i = j = 0;
     while(i < n) {
         if(pat[j] == txt[i]) {
             i++; 
@@ -35,8 +34,10 @@ int KMP(char *txt, char *pat) {
             printf("Pattern found at index %d\n", i - j + 1);
             j = lps[j - 1];
         } else if(i < n && pat[j] != txt[i]) {
-            if(j != 0) j = lps[j - 1];
-            else i++;
+            if(j != 0) 
+                j = lps[j - 1];
+            else 
+                i++;
         }
     }
 }
